a03dde437095064679b9bde471b4fddfc9f65144
ajay sieunarine <ajay_sieunarine@hotmail.com>
2025-02-21 02:40:24 -0400

    fix runner for windows

--- scraper/runner.bat
+++ scraper/runner.bat
@@ -1,4 +1,17 @@
 @echo off
-# CALL "C:\ProgramData\anaconda3\Scripts\activate.bat" root
+REM Change directory to the script location
+cd /d %~dp0
+
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Install dependencies from requirements.txt (if available)
+pip install -r requirements.txt
+
+REM Alternatively, install numpy specifically if you don't have a requirements file
+REM pip install numpy
+
+REM Run the Python script
 python main.py
+
 pause
